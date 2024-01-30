c653cc917709382a5adff2836b41a53dff1c61fd
Aaryan-Pala <aaryanpala05@gmail.com>
2024-01-31 10:46:11 +1100

    See earlier commit

--- Loch/Contact Group/Contacts.swift
+++ Loch/Contact Group/Contacts.swift
@@ -17,24 +17,26 @@ import SwiftUI
 
 struct Contacts: View {
     var body: some View {
-        List(contacts) { contact in
-            HStack(){
-                Image(contact.imageName)
-                    .resizable()
-                    .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-                    .frame(width: 60, height: 60)
-                    .clipped()
-                    .cornerRadius(50)
-                VStack(alignment: .leading, spacing: 50) {
-                    Text(contact.name)
-                        .font(.system(size: 21, weight: .medium, design: .default))
-                    
+        NavigationView() {
+            List(contacts) { contact in
+                NavigationLink(destination: Detailed_View(contact: contact)) {
+                    HStack(){
+                        Image(contact.imageName)
+                            .resizable()
+                            .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                            .frame(width: 60, height: 60)
+                            .clipped()
+                            .cornerRadius(50)
+                        VStack(alignment: .leading, spacing: 50) {
+                            Text(contact.name)
+                                .font(.system(size: 21, weight: .medium, design: .default))
+                            
+                        }
+                    }
                 }
             }
-            
+            .navigationTitle("Contacts")
         }
-        .position(CGPoint(x: 200, y: 500))
-        .flipsForRightToLeftLayoutDirection(true)
     }
 }
 
--- Loch/Contact Group/Detailed View.swift
+++ Loch/Contact Group/Detailed View.swift
@@ -8,11 +8,45 @@
 import SwiftUI
 
 struct Detailed_View: View {
+    
+    let contact: contactData
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Image(contact.imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 150, height: 150)
+                .clipped()
+                .cornerRadius(150)
+                .shadow(radius: 3)
+            Text(contact.name)
+                .font(.title)
+                .fontWeight(.bold)
+            Form {
+                HStack(){
+                    Text("Name")
+                    Spacer()
+                    Text(contact.name)
+                        .foregroundColor(.gray)
+                        .font(.callout)
+                }
+                Section {
+                    Button(action: {
+                        
+                    }) {
+                        Text("Send Message")
+                    }
+                }
+                
+            }
+            
+            
+            
+        }
     }
 }
 
 #Preview {
-    Detailed_View()
+    Detailed_View(contact: contacts[0])
 }
