befc4b2323b9632bfa320b81b4ca46ea6456e820
Aaryan-Pala <aaryanpala05@gmail.com>
2024-01-30 13:10:33 +1100

    Fundamental UI updates

---
+++ Loch/Settings.swift
@@ -0,0 +1,18 @@
+//
+//  Settings.swift
+//  Loch
+//
+//  Created by A P on 30/1/2024.
+//
+
+import SwiftUI
+
+struct Settings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Settings()
+}
