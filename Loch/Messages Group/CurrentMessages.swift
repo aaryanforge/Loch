dfae0dc4e20bc0b140a6adad7772469451328d80
Aaryan-Pala <aaryanpala05@gmail.com>
2024-02-14 18:02:35 +1100

    Updated messages system

---
+++ Loch/Messages Group/CurrentMessages.swift
@@ -0,0 +1,9 @@
+//
+//  CurrentMessages.swift
+//  Loch
+//
+//  Created by A P on 14/2/2024.
+//
+
+import Foundation
+import SwiftUI
--- Loch/Messages Group/MessagesPage.swift
+++ Loch/Messages Group/MessagesPage.swift
@@ -8,25 +8,51 @@
 import SwiftUI
 
 
+
+struct currentMsgs {
+    var cMsg: String
+}
+
 struct MessagesPage: View {
         
     var msg: messagesData
     @State private var txtMsg: String = ""
     @State var currentMsg: String = ""
-    @State var messaged = false
+    var count = 1 ... 10
+    
+    @State var Ms = [currentMsgs(cMsg: ""), currentMsgs(cMsg: ""), currentMsgs(cMsg: ""), currentMsgs(cMsg: ""), currentMsgs(cMsg: "")]
     
     func makeTxt(txtMsg: String) -> Text {
         return Text(txtMsg)
     }
     
     var body: some View {
-        Text(currentMsg)
+        Section() {
+            Text(Ms[0].cMsg)
+        }
+        Section() {
+            Text(Ms[1].cMsg)
+        }
+        Section() {
+            Text(Ms[2].cMsg)
+        }
+        Section() {
+            Text(Ms[3].cMsg)
+        }
+        Section() {
+            Text(Ms[4].cMsg)
+        }
         
         TextField("Message @" + msg.name, text: $txtMsg)
             .onSubmit {
-                print(txtMsg)
+                var count = 0 ... Ms.count
+                for i in count {
+                    if Ms[(i)].cMsg == "" {
+                        Ms[(i)].cMsg = txtMsg
+                        break
+                    }
+                }
                 currentMsg = txtMsg
-                messaged = true
             }
             .border(Color.black)
     }
