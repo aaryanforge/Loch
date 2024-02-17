fbdc1a620097dd9cd79a0e6c23c7f8fb0dee977f
0iselegant <0iselegant@gmail.com>
2024-02-17 15:46:01 +1100

    LoginView created

---
+++ Loch/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  Loch
+//
+//  Created by Phoenix Liu on 17/2/2024.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text("Hi Penghus")
+    }
+}
+
+#Preview {
+    LoginView()
+}
