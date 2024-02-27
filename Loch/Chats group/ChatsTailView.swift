c9614b84d9e22ef910bcf04c8b2bcd0b83341898
0iselegant <0iselegant@gmail.com>
2024-02-27 20:27:29 +1100

    Mark's work

--- Loch/Chats group/ChatsTailView.swift
+++ Loch/Chats group/ChatsTailView.swift
@@ -5,6 +5,10 @@
 //  Created by Phoenix Liu on 25/2/2024.
 //
 
+// Goal: Create a textfield that sends the user messages and cache to memory
+// For: Mark
+// Due: 29 Thu Feb
+
 import SwiftUI
 
 struct ChatsTailView: View {
