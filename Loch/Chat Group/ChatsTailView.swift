a6374a5cea4077c18c63b1a66510ea6be5cc0b80
0iselegant <0iselegant@gmail.com>
2024-03-04 22:13:40 +1100

    marks changes

--- Loch/Chat Group/ChatsTailView.swift
+++ Loch/Chat Group/ChatsTailView.swift
@@ -16,6 +16,28 @@ struct ChatsTailView: View {
     
     @Environment(\.modelContext) private var context
     @State private var newTextMessage: String = ""
+//<<<<<<< Updated upstream
+//=======
+//<<<<<<< Updated upstream
+    @StateObject var ChatMessageContentVM = MessageContentViewModel()
+
+//    context.insert([MessageBeingTyped])
+    func cacheNewMessage() async {
+        // use URLSession API call to see whether to cache the message or not
+        // guard that the message classification is a 1, else cache message
+        
+        var messageClassification = await ChatMessageContentVM.getData(newTextMessage)
+        
+        if (messageClassification == "0") {
+            var newMessage = ChatTextMessage(
+                senderID: UUID().uuidString,
+                messageContents: newTextMessage
+            )
+            context.insert(newMessage)
+        }
+        //=======
+    }
+//>>>>>>> Stashed changes
     
     func cacheNewMessage() {
         var newMessage = ChatTextMessage(
