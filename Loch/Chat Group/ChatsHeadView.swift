75c6699df93f94905bfae4bab33a4c4b13ed3705
jeffery-jefferson <mark.thomas.ureta@gmail.com>
2024-03-05 22:57:24 +1100

    started on caching forum

--- Loch/Chat Group/ChatsHeadView.swift
+++ Loch/Chat Group/ChatsHeadView.swift
@@ -10,13 +10,42 @@
 // Due: Thursday 7th March
 
 import SwiftUI
+import SwiftData
 
 struct ChatsHeadView: View {
+    
+    @State private var textSearch = ""
+    @Query private var messages: [ChatTextMessage]
+    
+    // TODO: as messages from every person is cached, figure out a way to filter messages of the current person user is talking to
+    
     var body: some View {
-        Text("Hello, World!")
+        TextField(
+            "SearchQuery",
+            text: $textSearch
+        )
+        
+        Button {
+            // do something
+        } label: {
+            Text("<Enter>") // make into actual UI
+        }
+
     }
 }
 
 #Preview {
     ChatsHeadView()
+}
+
+// returns a list of messages that belong to the current user
+func getCurrentUserCachedMessages(of currentUserID: UUID, using allMessages: [ChatTextMessage]) -> [ChatTextMessage] {
+    
+    return []
+}
+
+// finds and returns the message from a serach query
+func getMessagesFromQuery(find messageQuery: String, from messages: [ChatTextMessage]) /*-> ChatTextMessage*/ {
+    
+    return
 }
