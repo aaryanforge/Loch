74300f7bc8f23200aaf6367e1264357c3a30809a
Mary <maryduan@outlook.com.au>
2024-03-15 00:30:36 +1100

    made basic profile settings page

--- Loch/Profile Group/ProfileMainView.swift
+++ Loch/Profile Group/ProfileMainView.swift
@@ -36,7 +36,6 @@ struct ProfilePictureCircle: View {
 
     var body: some View {
         ZStack {
-
             if (imageLink != nil){
                 Image(imageLink)
                     .resizable()
@@ -49,13 +48,12 @@ struct ProfilePictureCircle: View {
                     .aspectRatio(CGSize(width:1, height: 1), contentMode: .fit)
                     .frame(width: size, height: size)
                     .clipShape(Circle())
-
             }
 
             if (hasBorder){
                 Circle()
                     .strokeBorder(.green, style:
-                        StrokeStyle(lineWidth: 1.5, miterLimit: 10, dash: [20, 5], dashPhase: 5)
+                                    StrokeStyle(lineWidth: 1.5, miterLimit: 10, dash: [20, 5], dashPhase: 5)
                     )
             }
         }
@@ -74,41 +72,39 @@ struct ButtonSquare: View {
             self.todo()
         } label: {
             ZStack{
-                RoundedRectangle(cornerRadius: 15, style: .continuous)
-                    .background(.green)
-                Label(actionName, systemImage: image)
+                RoundedRectangle(cornerRadius: 8, style: .continuous)
+                .fill(.green)
+                .frame(width: size, height: size)
+
+                Image(systemName: image)
                     .foregroundColor(.white)
             }
-            .frame(maxWidth: size)
         }
-        .padding(8)
+        .padding(2)
     }
 }
-
 struct ProfileAboutInfo: View {
-    var profPicLink: String
+    var profPicLink: String?
     var name: String
     var bio: String
 
     var body: some View {
         VStack{
-
             // top half of profile banner
             HStack {
-                ProfilePictureCircle(imageLink: profPicLink, size: 120, hasBorder: true)
-                .padding()
+                ProfilePictureCircle(size: 130, hasBorder: true )
+                    .padding()
 
                 //name and buttons
-                VStack{
+                VStack(alignment: .leading){
                     Text("AKA")
                         .foregroundColor(.gray)
                         .font(.system(size: 14,weight: .medium, design: .default))
-                        .padding(8)
                     Text(name)
                         .foregroundColor(.black)
                         .font(.system(size: 17,weight: .semibold, design: .default))
-                        .padding(10)
-                    
+                        .padding([.bottom])
+
                     HStack {
                         ButtonSquare(todo: {print("Edit")}, actionName: "Edit", image: "square.and.pencil", size: 40)
                         ButtonSquare(todo: {print("Notifications")}, actionName: "Notifications", image: "bell", size: 40)
@@ -116,44 +112,48 @@ struct ProfileAboutInfo: View {
 
                 }
                 .padding()
-
-                //bio part
-                Text(bio)
-                    .foregroundColor(.black)
-                    .frame(maxWidth: 130)
-                    .background(.green.opacity(0.2))
-                    .clipShape(RoundedRectangle(cornerRadius: 15))
-                    .padding(10)
             }
+            //bio part
+
+            Text(bio)
+                .foregroundColor(.black)
+                .frame(maxWidth: .infinity, maxHeight: 130, alignment: .topLeading)
+                .padding()
+                .background(.green.opacity(0.2)).clipShape(RoundedRectangle(cornerRadius: 8))
         }
+        .padding(10)
     }
 }
-
 struct ProfileMainView: View {
     var body: some View {
-        VStack {
+        VStack (alignment: .leading) {
             ProfileHeader(nickName: "Lochie")
 
             // main profile section
-            ProfileAboutInfo(profPicLink: "Connecting Image", name: "Lochilon Ness", bio: "peepee poopoo")
+            ProfileAboutInfo(profPicLink, "Connecting Image", name: "Lochilon Ness", bio: "peepee poopoo")
 
             //forum posts
             // should be renedered in reverse chronilogical order, with the newest ones first
-            LazyVStack{
-            ForumBlockPostPublicPrivate(
-                imageLink: "Connecting Image", 
-                title: "HELLO WORLD",
-                //if this is throwing a bug you may have to put this all on 1 line
-                postContent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 
-                postType: "priv"
-            )
 
-            ForumBlockPostPublicPrivate(
-                title: "Homework Help Q4",
-                postContent: "Can anybody help out with my maths homework??? \nI don't get Question 4", 
-                postType: "priv"
-            )
+            Text("Forum Posts:")
+                .font(.subheadline)
+                .padding([.leading])
+            LazyVStack{
+                ForumBlockPostPublicPrivate(
+                    imageLink: "Connecting Image", 
+                    title: "HELLO WORLD",
+                    //if this is throwing a bug you may have to put this all on 1 line
+                    postContent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 
+                    postType: "priv"
+                )
+
+                ForumBlockPostPublicPrivate(
+                    title: "Homework Help Q4",
+                    postContent: "Can anybody help out with my maths homework??? \nI don't get Question 4", 
+                    postType: "priv"
+                )
             }
+            .padding(10)
         }
     }
 }
--- Loch/Profile Group/ProfileSettingsView.swift
+++ Loch/Profile Group/ProfileSettingsView.swift
@@ -11,11 +11,131 @@ import SwiftUI
 // For: Mary
 // Due: Thursday 14th Mar
 
+
+func goBack() { print ("goBack") }
+func submit() { print ("submit") }
+
+struct AvatarSection: View {
+    var profPicLink: String
+
+    var body: some View {
+        VStack{
+            ZStack {
+
+                //profile picture
+                ProfilePictureCircle(imageLink: profPicLink, size: 120, hasBorder: true)
+                    .padding()
+
+                // grey overlay
+                Circle()
+                    .fill(.black.opacity(0.2))
+                Image(systemName: "square.and.pencil")
+                    .foregroundColor(.white)
+            }
+            .padding(10)
+
+            Text("Edit Avatar")
+                .foregroundColor(.green)
+                .font(.system(size: 8 ,weight: .light, design: .default))
+        }
+        .padding()
+    }
+}
+
 struct ProfileDetailView: View {
+    @State private var nickname: String = ""
+    @State private var realName: String = ""
+    @State private var bio: String = ""
+
     var body: some View {
         VStack {
-            // Mary's code
+            VStack (alingment: left) {
+                //back button
+                Button {
+                    goBack()
+                } label: {
+                    Image(systemName: "arrow.left")
+                        .foregroundColor(.gray)
+                }
+                .padding(8)
+            }
+
+            AvatarSection(profPicLink: "Connecting Image")
+
+            //text enter fields
+            VStack {
+                //Nickname
+                VStack (alignment: .leading){
+                    Text("Nickname")
+                        .foregroundColor(.green)
+                        .font(.system(size: 15 ,weight: .regular, design: .default))
+                        .padding(5)
+
+                    TextField(
+                        "Lochie",
+                        text: $nickname 
+                    )
+                        .padding(10)
+                        .foregroundColor(.white)
+                        .background(.green.opacity(0.2), in: RoundedRectangle (cornerRadius: 8, style: .continuous))
+                        .autocorrectionDisabled(false)
+                }
+                .padding()
+
+                //Real Name
+                VStack (alignment: .leading){
+                    Text("Real Name")
+                        .foregroundColor(.green)
+                        .font(.system(size: 15 ,weight: .regular, design: .default))
+                        .padding(5)
+                
+                    TextField(
+                        "Lochilon Ness",
+                        text: $realName
+                    )
+                        .padding(10)
+                        .foregroundColor(.white)
+                        .background(.green.opacity(0.2), in: RoundedRectangle (cornerRadius: 8, style: .continuous))
+                        .autocorrectionDisabled(false)
+                }
+                .padding()
+
+                //Bio
+                VStack (alignment: .leading){
+                    Text("About You")
+                        .foregroundColor(.green)
+                        .font(.system(size: 15 ,weight: .regular, design: .default))
+                        .padding(5)
+                
+                    TextField(
+                        "blah blach",
+                        text: $bio
+                    )
+                        .padding(10)
+                        .frame(maxHeight: 100, alignment: .top)
+                        .foregroundColor(.white)
+                        .background(.green.opacity(0.2), in: RoundedRectangle (cornerRadius: 8, style: .continuous))
+                        .autocorrectionDisabled(false)
+                }
+                .padding()
+            }
+
+            //submit button
+           Button {
+                    submit()
+                } label: {
+                    ZStack{
+                        RoundedRectangle (cornerRadius: 8, style: .continuous)
+                            .fill(.green)
+                        Text("Submit")
+                            .foregroundColor(.white)
+                    }
+                    .frame(maxWidth: 80, maxHeight: 50)
+                }
+                .padding(8)
+
         }
+        .padding()
         
     }
 }
